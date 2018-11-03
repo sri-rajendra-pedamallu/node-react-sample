5c44a2d57f1a9f53b48152ec7f541ce0d6cb2867
sri rajendra <rajendra0482@gmail.com>
2018-11-02 23:18:30 -0700

    initial db setup

---
+++ backend/databasePool.js
@@ -0,0 +1,11 @@
+const {Pool} = require('pg');
+const databaseConfiguration = require('./secrets/databaseConfiguration');
+
+const pool = new Pool(databaseConfiguration);
+
+module.exports = pool;
+
+// pool.query('SELECT * FROM generation',(error,response) => {
+//     if(error) console.log("error",error);
+//     console.log('response.rows',response.rows)
+// })
